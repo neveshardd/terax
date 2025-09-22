28cd18f079afd99cc1b0d646bc7281136fe676e9
neves <118855875+neveshardd@users.noreply.github.com>
2025-09-22 20:18:23 -0300

    change envs

--- apps/fastify/src/server.js
+++ apps/fastify/src/server.js
@@ -14,8 +14,8 @@ app.use(express.json())
 
 app.use(cors({
     origin: [
-        'http://localhost:3000',
-        'http://localhost:3001'
+        `${process.env.PUBLIC_AUTH_URL}` || 'http://localhost:3000',
+        `${process.env.PUBLIC_STORE_URL}` || 'http://localhost:3001'
     ],
     credentials: true,
     methods: ['GET', 'POST']
